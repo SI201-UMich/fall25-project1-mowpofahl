82f31f762468f8808452d756dd1581655d076539
MoPofahl <112831968+MoPofahl@users.noreply.github.com>
2025-10-15 19:18:04 -0400

    calculation functions

--- Project1.py
+++ Project1.py
@@ -19,3 +19,11 @@ def calculate_mean_body_mass_by_sex(penguin_data):
 def calculate_mean_body_mass_by_species(penguin_data):
     mean_body_mass_by_species = penguin_data.groupby('species')['body_mass_g'].mean().to_dict()
     return mean_body_mass_by_species
+
+def calculate_mean_bill_length_by_species(penguin_data):
+    mean_bill_lengths_by_species = penguin_data.groupby('species')['bill_length_mm'].mean().to_dict()
+    return mean_bill_lengths_by_species
+
+def calculate_correlation_bill_length_and_depth(penguin_data):
+    correlation = penguin_data['bill_length_mm'].corr(penguin_data['bill_depth_mm'])
+    return correlation
