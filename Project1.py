58538e7d4ba7c81a519b0a497d65885ec9e60d89
MoPofahl <112831968+MoPofahl@users.noreply.github.com>
2025-10-15 19:16:48 -0400

    First two calculations

--- Project1.py
+++ Project1.py
@@ -12,3 +12,10 @@ def load_data(csv_file):
     penguin_data = pd.read_csv(csv_file)
     return penguin_data
 
+def calculate_mean_body_mass_by_sex(penguin_data):
+    mean_body_mass_by_sex = penguin_data.groupby('sex')['body_mass_g'].mean().to_dict()
+    return mean_body_mass_by_sex
+
+def calculate_mean_body_mass_by_species(penguin_data):
+    mean_body_mass_by_species = penguin_data.groupby('species')['body_mass_g'].mean().to_dict()
+    return mean_body_mass_by_species
